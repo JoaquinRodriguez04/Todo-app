b05715848e1460d87091ae3b7e37e2e4b0e15f86
Joaquin <JoaquindeveloperWeb@gmail.com>
2023-08-16 21:03:34 -0300

    se agrego el componente que contiene el banner del centro de control

---
+++ src/components/BannerTodo.jsx
@@ -0,0 +1,12 @@
+import iconMoon from '../assets/icon-moon.svg';
+
+const BannerTodo = () => {
+  return (
+    <div className='banner-todo-container'>
+        <h2>TODO</h2>
+        <img src={iconMoon} alt="icon-dak-mode" />
+    </div>
+  )
+};
+
+export default BannerTodo;
