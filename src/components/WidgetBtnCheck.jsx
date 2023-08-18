2ee2744617e6722802f463a66c13e65356448265
Joaquin <JoaquindeveloperWeb@gmail.com>
2023-08-17 21:05:19 -0300

    se agrego el widget que ejecuta la logica si se hace un click en el check del todo o tarea

---
+++ src/components/WidgetBtnCheck.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { TodoContext } from "./TodoContext";
+import WidgetChecked from "./widgetChecked";
+import WidgetEmpty from "./WidgetEmpty";
+
+const WidgetBtnCheck = ({todo, index}) => {
+
+    const {handleSetCheck} = useContext(TodoContext);
+
+  return (
+    <button 
+    className="todo-item-btn-widget"
+    onClick={() => handleSetCheck(index)}>
+        {todo.checked ? <WidgetChecked/> : <WidgetEmpty/>}
+    </button>
+   
+  )
+};
+
+export default WidgetBtnCheck;
