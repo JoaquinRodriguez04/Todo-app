6924c42037eac1beef3f06058bc6ea33912b8898
Joaquin <JoaquindeveloperWeb@gmail.com>
2023-08-21 01:23:40 -0300

    se actualizo el componente

--- src/components/HeroTodo.jsx
+++ src/components/HeroTodo.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { TodoContext } from "./TodoContext";
 
 const HeroTodo = () => {
+
+  const {mode, animationMode} = useContext(TodoContext);
+
   return (
-    <div className='hero-todo-container'>
+    <div className={`hero-todo-container ${mode && 'image-mode-dark'} ${animationMode && 'image-mode-animation'}`}>
       {/* contiene la imagen de fondo */}
     </div>
   )
