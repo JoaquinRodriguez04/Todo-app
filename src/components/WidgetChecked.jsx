e7ef64ffdde64d314fe1a7d6abe5a389232ba3ac
Joaquin <JoaquindeveloperWeb@gmail.com>
2023-08-21 01:24:32 -0300

    se actualizo el componente

--- src/components/WidgetChecked.jsx
+++ src/components/WidgetChecked.jsx
@@ -1,8 +1,13 @@
+import { useContext } from 'react';
 import iconCheck from '../assets/icon-check.svg';
+import { TodoContext } from './TodoContext';
 
 const WidgetChecked = () => {
+
+  const {mode} = useContext(TodoContext);
+
   return (
-    <div className='todo-circle todo-circle-checked'>
+    <div className={`todo-circle todo-circle-checked ${mode && 'todo-circle-dark-mode'}`}>
         <img src={iconCheck} className='img-check' alt="icon-check" />
     </div>
   )
