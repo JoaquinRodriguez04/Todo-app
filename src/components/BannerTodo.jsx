fee0b31cfa0e817afc54a5364b01c14f6a50a539
Joaquin <JoaquindeveloperWeb@gmail.com>
2023-08-21 01:22:27 -0300

    se actualizo el componente

--- src/components/BannerTodo.jsx
+++ src/components/BannerTodo.jsx
@@ -1,10 +1,28 @@
+// icono para modo claro
+import iconLight from '../assets/icon-sun.svg';
+
+// icono para modo oscuro
 import iconMoon from '../assets/icon-moon.svg';
 
+import { TodoContext } from './TodoContext';
+import { useContext } from 'react';
+
 const BannerTodo = () => {
+  
+  const {mode, setMode, animationMode, setAnimationMode} = useContext(TodoContext);
+
+  const handleSetMode = () => {
+    setMode(!mode);
+    setAnimationMode(false);
+  };
+
   return (
     <div className='banner-todo-container'>
-        <h2>TODO</h2>
-        <img src={iconMoon} alt="icon-dak-mode" />
+        <h2 className='banner-title'>TODO</h2>
+        <button
+        onClick={handleSetMode}>
+          <img src={mode ? iconLight : iconMoon} className={`${animationMode && 'banner-todo-icon-mode'}`} alt="icon-dak-mode" />
+        </button>
     </div>
   )
 };
